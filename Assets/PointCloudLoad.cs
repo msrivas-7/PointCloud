6ecaf500fcfe0364ec9c21bee197952dd858914d
msrivas-7 <msrivas7@asu.edu>
2023-01-06 13:36:25 -0700

    Data

---
+++ Assets/PointCloudLoad.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class PointCloudLoad : MonoBehaviour
+{
+    Texture2D texColor;
+    Texture2D texPosScale;
+    VisualEffect vfx;
+    uint resolution = 1024;
+
+
+    public MeshFilter mesh;
+    public float particleSize = 0.1f;
+    bool toUpdate = false;
+    uint particleCount = 0;
+
+    private void Start()
+    {
+        vfx = GetComponent<VisualEffect>();
+        vfx.SetUInt(Shader.PropertyToID("Resolution"), resolution);
+
+        Vector3[] positions = mesh.sharedMesh.vertices;
+        Color[] colors = mesh.sharedMesh.colors;
+
+        Debug.Log("Rendering " + positions.Length + " Particles");
+        SetParticles(positions, colors);
+    }
+
+
+    private void Update()
+    {
+        if (toUpdate)
+        {
+            toUpdate = false;
+
+            vfx.Reinit();
+            vfx.SetUInt(Shader.PropertyToID("ParticleCount"), particleCount);
+            vfx.SetTexture(Shader.PropertyToID("TexColor"), texColor);
+            vfx.SetTexture(Shader.PropertyToID("TexPosScale"), texPosScale);
+            vfx.SetUInt(Shader.PropertyToID("Resolution"), resolution);
+        }
+    }
+
+    public void SetParticles(Vector3[] positions, Color[] colors)
+    {
+        texColor = new Texture2D(positions.Length > (int)resolution ? (int)resolution : positions.Length, Mathf.Clamp(positions.Length / (int)resolution, 1, (int)resolution), TextureFormat.RGBAFloat, false);
+        texPosScale = new Texture2D(positions.Length > (int)resolution ? (int)resolution : positions.Length, Mathf.Clamp(positions.Length / (int)resolution, 1, (int)resolution), TextureFormat.RGBAFloat, false);
+        int texWidth = texColor.width;
+        int texHeight = texColor.height;
+
+        for (int y = 0; y < texHeight; y++)
+        {
+            for (int x = 0; x < texWidth; x++)
+            {
+                int index = x + y * texWidth;
+                // Assign a random color to each point
+                Color randomColor = new Color(Random.value, Random.value, Random.value, 1.0f);
+                texColor.SetPixel(x, y, randomColor);
+                var data = new Color(positions[index].x, positions[index].y, positions[index].z, particleSize);
+                texPosScale.SetPixel(x, y, data);
+            }
+        }
+
+        texColor.Apply();
+        texPosScale.Apply();
+        particleCount = (uint)positions.Length;
+        toUpdate = true;
+    }
+
+}
